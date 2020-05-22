f79e493e418076ce6a5b47780c756e2019903648
unknown <fe97964@gmail.com>
2020-05-22 10:20:31 -0300

    Tony e Riri

---
+++ Desktop/Primeiro POO/Personagem.cs
@@ -0,0 +1,16 @@
+namespace Primeiro_POO
+{
+    public class Personagem
+    {
+        public string nome = "Tony Stark";
+        public int idade = 35;
+        public string armadura;
+
+        public string Atacar(){
+            return "O personagem atacou";
+        }
+
+        public string name = "Riri Williams";
+        
+    }
+}
---
+++ Desktop/Primeiro POO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Primeiro_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem tony = new Personagem();
+            tony.armadura = "Bleeding Edge";
+            Console.WriteLine(tony.nome);
+            Console.WriteLine(tony.armadura);
+            Console.WriteLine(tony.Atacar());
+
+            Console.WriteLine ();
+
+           Personagem riri = new Personagem();
+           riri.armadura = "Iron Heart";
+           Console.WriteLine (riri.name);
+           Console.WriteLine (riri.armadura);
+           Console.WriteLine (riri.Atacar());
+    }
+}
+        }
