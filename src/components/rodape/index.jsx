1280aec79123fe4bdb3f0033f1ab4dc17949123a
Vinicius <viniciusokaeda@gmail.com>
2020-10-27 08:46:38 -0300

    Componentes feitos

---
+++ src/components/rodape/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rodape = () => {
+    return (
+        <footer className="text-center" style={{marginTop : '70px' }}>
+            <h1>Edu X</h1>
+            <small>Desenvolvido pela equipe Alpha</small>
+        </footer>
+    )
+}
+
+export default Rodape;
