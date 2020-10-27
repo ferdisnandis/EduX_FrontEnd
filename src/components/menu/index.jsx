add8ae5d3a9ff9509a9c2bbc068929f23c8598fe
Vinicius <viniciusokaeda@gmail.com>
2020-10-27 10:12:36 -0300

    Menu/Rodapé

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import logo from '../../assets/img/logo_2.png';
 
 const Menu = () => {
     return (
         <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Edu X</Navbar.Brand>
+        <Navbar.Brand href="/"><img src={logo} alt='Logo da EduX' style={{ width: '110px'}} /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            
         </Nav>
-        <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/cadastrar">cadastrar</Nav.Link>
-        </Nav>
+            <Nav.Link href="/cadastrar">Cadastrar</Nav.Link>
+        
         </Navbar.Collapse>
       </Navbar>
     )
--- src/components/rodape/index.jsx
+++ src/components/rodape/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../../assets/img/logo_2.png';
 
-const Rodape = () => {
+
+const Rodape  = () => {
     return (
-        <footer className="text-center" style={{marginTop : '70px' }}>
-            <h1>Edu X</h1>
+        <footer className="text-center " style={{marginTop : '70px' }}>
+            <h1 href="/"><img src={logo} alt='Logo da EduX' style={{ width: '150px' }} /></h1>
             <small>Desenvolvido pela equipe Alpha</small>
         </footer>
     )
